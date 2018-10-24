d1c7373d6074273a1d2d1f13e81eadf01813aa06
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2018-10-24 20:35:52 +0200

    ga, search console

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,6 +17,13 @@ module.exports = {
 			description: 'New typefaces for Warsaw'
 		}
 	},
+
+	ga: 'UA-87426844-1',
+
+	head: [
+		['meta', { name: 'google-site-verification', content: 'gHY6y9T0nYLaIqJn6P4_oY9RKYL_JfLmqPu-ZaI4W3k' }]
+	],
+
 	postcss: {
 		plugins: [
 			require('postcss-preset-env')({}),
