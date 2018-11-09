f004720726ac8ffca6a3ac3c798041c077217d9a
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2018-11-10 00:24:14 +0100

    added ga

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,9 +18,9 @@ module.exports = {
 		}
 	},
 
-	ga: 'UA-87426844-1',
-
 	head: [
+		['script', {async: true, src: 'https://www.googletagmanager.com/gtag/js?id=UA-87426844-1'}],
+		['script', {src: '/js/ga.js'}],
 		['meta', { name: 'google-site-verification', content: 'gHY6y9T0nYLaIqJn6P4_oY9RKYL_JfLmqPu-ZaI4W3k' }],
 		['meta', { name: 'image', content: 'https://kroje.org/assets/img/warszawskie-kroje-00.7790f955.jpg' }],
 		['meta', { name: 'og:image', content: 'https://kroje.org/assets/img/warszawskie-kroje-00.7790f955.jpg'}],
