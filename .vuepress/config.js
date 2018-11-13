0c040c69f3c5db02e64b73bb17afc50d170efd09
Jan Franciszek Cieślak <jfcieslak@gmail.com>
2018-11-13 15:39:22 +0100

    meta

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,7 +3,7 @@ const themeConfig = require('./themeConfig')
 module.exports = {
 	title: 'Warszawskie Kroje',
 	
-	description: 'Nowe kroje dla Warszawy',
+	description: 'Warszawskie Kroje to 12 nowych, darmowych krojów pism stworzonych specjalnie dla Warszawy, w oparciu o lokalne tradycje typograficzne.',
 	
 	themeConfig: themeConfig,
 
@@ -22,8 +22,19 @@ module.exports = {
 		['script', {async: true, src: 'https://www.googletagmanager.com/gtag/js?id=UA-87426844-1'}],
 		['script', {src: '/js/ga.js'}],
 		['meta', { name: 'google-site-verification', content: 'gHY6y9T0nYLaIqJn6P4_oY9RKYL_JfLmqPu-ZaI4W3k' }],
-		['meta', { name: 'image', content: 'https://kroje.org/assets/img/warszawskie-kroje-00.7790f955.jpg' }],
-		['meta', { name: 'og:image', content: 'https://kroje.org/assets/img/warszawskie-kroje-00.7790f955.jpg'}],
+		['meta', { name: 'image', content: 'https://kroje.org/images/og-image.png' }],
+		//  Facebook
+		['meta', { name: 'og:title', content: 'Warszawskie Kroje' }],
+		['meta', { name: 'og:description', content: 'Warszawskie Kroje to 12 nowych, darmowych krojów pism stworzonych specjalnie dla Warszawy, w oparciu o lokalne tradycje typograficzne.' }],
+		['meta', { name: 'og:type', content: 'website' }],
+		['meta', { name: 'og:url', content: 'https://kroje.org/' }],
+		['meta', { name: 'og:image', content: 'https://kroje.org/images/og-image.png'}],
+		// Twitter
+		['meta', { name: 'twitter:title', content: 'Warszawskie Kroje' }],
+		['meta', { name: 'twitter:description', content: 'Warszawskie Kroje to 12 nowych, darmowych krojów pism stworzonych specjalnie dla Warszawy, w oparciu o lokalne tradycje typograficzne.' }],
+		['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+		['meta', { name: 'twitter:url', content: 'https://kroje.org/' }],
+		['meta', { name: 'twitter:image', content: 'https://kroje.org/images/og-image.png'}],
 	],
 
 	postcss: {
